2b094c148d26a4302201eaaa37b76a00d65af18e
tanyshkakoval28@gmail.com <tanyshkakoval28@gmail.com>
2024-06-26 14:31:57 +0300

    button ui component

---
+++ src/components/ui/button/button.tsx
@@ -0,0 +1,38 @@
+import { DOMAttributes, FC, MouseEvent, ReactNode } from 'react';
+import { TButton, TClasses } from './types';
+import Icon, { IconType } from '../icon';
+
+export interface IButtonProps extends DOMAttributes<HTMLButtonElement> {
+  className?: string;
+  isActive?: boolean;
+  isDisabled?: boolean;
+  onClick?: (event?: MouseEvent<HTMLButtonElement>) => void;
+  children: ReactNode;
+  icon?: IconType;
+  classes?: TClasses;
+  type?: TButton;
+}
+
+const Button: FC<IButtonProps> = ({
+  icon,
+  // isActive = true,
+  isDisabled = false,
+  children,
+  classes,
+  type = 'button',
+  onClick,
+}) => {
+  return (
+    <button
+      data-value={children}
+      className={classes?.button}
+      disabled={isDisabled}
+      onClick={onClick}
+      type={type}
+    >
+      {children}
+      {icon && <Icon className={classes?.icon} type={icon} />}
+    </button>
+  );
+};
+export default Button;
