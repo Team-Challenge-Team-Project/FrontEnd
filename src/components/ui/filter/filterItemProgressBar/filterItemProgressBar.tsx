87eaccb51c9597edc140104291309cfd3394681a
gomunkool <pivzz19881988@gmil.com>
2024-07-06 14:40:10 +0200

    add inputs

--- src/components/ui/filter/filterItemProgressBar/filterItemProgressBar.tsx
+++ src/components/ui/filter/filterItemProgressBar/filterItemProgressBar.tsx
@@ -1,22 +1,42 @@
 import './filterItemProgressBar.style.css'
 import { TProgressBarItem } from '../types'
+import React, { useState } from 'react'
 
 interface FilterProgressBarProps {
   progressBarItems: TProgressBarItem;
 }
 
 export const FilterItemProgressBar = ({ progressBarItems }: FilterProgressBarProps) => {
+  const [startValue, setStartValue] = useState<string>(String(''));
+  const [endValue, setEndValue] = useState<string>(String(''));
+
+  const handleNumericInput = (e: React.ChangeEvent<HTMLInputElement>, setValue: (value: string) => void) => {
+    const value = e.target.value;
+    if (/^\d*$/.test(value)) {
+      setValue(value);
+    }
+  };
+
   return (
     <div className='progress-bar_count'>
       <div className='progress-bar__window_count'>
-        <div
-          className='progress-bar__window progress-bar__window_min'>
-          {`${progressBarItems.currencies[0]} ${progressBarItems.start}`}
-        </div>
+        <input
+          type='text'
+          className='progress-bar__input progress-bar__input_min'
+          placeholder={`${progressBarItems.currencies[0]} ${progressBarItems.start}`}
+          value={startValue}
+          onChange={(e) => handleNumericInput(e, setStartValue)}
+          inputMode='numeric'
+        />
         <div className='progress-bar__window_pass'>-</div>
-        <div className='progress-bar__window progress-bar__window_max'>
-          {`${progressBarItems.currencies[0]} ${progressBarItems.end}`}
-        </div>
+        <input
+          type='text'
+          className='progress-bar__input progress-bar__input_max'
+          placeholder={`${progressBarItems.currencies[0]} ${progressBarItems.end}`}
+          value={endValue}
+          onChange={(e) => handleNumericInput(e, setEndValue)}
+          inputMode='numeric'
+        />
       </div>
       <div className='progress-bar__scroll_count'>
         <div className='progress-bar__scroll progress-bar__scroll_min'></div>
