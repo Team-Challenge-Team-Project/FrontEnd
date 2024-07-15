10c598b8b575a1879d13a662b4d3514adb731aaa
gomunkool <pivzz19881988@gmil.com>
2024-07-15 23:26:49 +0200

    add: ReactSlider

--- src/components/ui/filter/filterItemProgressBar/filterItemProgressBar.tsx
+++ src/components/ui/filter/filterItemProgressBar/filterItemProgressBar.tsx
@@ -1,21 +1,41 @@
 import './filterItemProgressBar.style.css'
 import { TProgressBarItem } from '../types'
 import React, { useState } from 'react'
+import ReactSlider from 'react-slider'
 
 interface FilterProgressBarProps {
   progressBarItems: TProgressBarItem;
 }
 
 export const FilterItemProgressBar = ({ progressBarItems }: FilterProgressBarProps) => {
-  const [startValue, setStartValue] = useState<string>(String(''));
-  const [endValue, setEndValue] = useState<string>(String(''));
+  const [startValue, setStartValue] = useState<string> (String (''))
+  const [endValue, setEndValue] = useState<string> (String (''))
 
-  const handleNumericInput = (e: React.ChangeEvent<HTMLInputElement>, setValue: (value: string) => void) => {
-    const value = e.target.value;
-    if (/^\d*$/.test(value)) {
-      setValue(value);
+
+  const handleNumericInputLeft = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value
+    if (/^\d*$/.test (value)) {
+      setStartValue (value)
     }
-  };
+    const exampleThumbLeft = document.querySelector ('.example-thumb-0') as HTMLElement
+    console.log (exampleThumbLeft.style.left)
+    console.log (value)
+    exampleThumbLeft.style.left = `${100}px`
+  }
+
+  const handleNumericInputRight = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value
+    if (/^\d*$/.test (value)) {
+      setEndValue (value)
+    }
+    const exampleThumbRight = document.querySelector ('.example-thumb-1')as HTMLElement
+    console.log (exampleThumbRight?.style.left)
+  }
+
+  const changeSlider = ({ value }: { value: number[], index: number }) => {
+    setStartValue (String (value[0] * 100))
+    setEndValue (String (value[1] * 100))
+  }
 
   return (
     <div className='progress-bar_count'>
@@ -25,7 +45,7 @@ export const FilterItemProgressBar = ({ progressBarItems }: FilterProgressBarPro
           className='progress-bar__input progress-bar__input_min'
           placeholder={`${progressBarItems.currencies[0]} ${progressBarItems.start}`}
           value={startValue}
-          onChange={(e) => handleNumericInput(e, setStartValue)}
+          onChange={(e) => handleNumericInputLeft (e)}
           inputMode='numeric'
         />
         <div className='progress-bar__window_pass'>-</div>
@@ -34,15 +54,24 @@ export const FilterItemProgressBar = ({ progressBarItems }: FilterProgressBarPro
           className='progress-bar__input progress-bar__input_max'
           placeholder={`${progressBarItems.currencies[0]} ${progressBarItems.end}`}
           value={endValue}
-          onChange={(e) => handleNumericInput(e, setEndValue)}
+          onChange={(e) => handleNumericInputRight (e)}
           inputMode='numeric'
         />
       </div>
-      <div className='progress-bar__scroll_count'>
-        <div className='progress-bar__scroll progress-bar__scroll_min'></div>
-        <div className='progress-bar__scroll_line'></div>
-        <div className='progress-bar__scroll progress-bar__scroll_max'></div>
-      </div>
+      <ReactSlider
+        className='horizontal-slider'
+        thumbClassName='example-thumb'
+        trackClassName='example-track'
+        defaultValue={[0, 10000]}
+        ariaLabel={['Lower thumb', 'Upper thumb']}
+        ariaValuetext={state => `Thumb value ${state.valueNow}`}
+        // renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
+        pearling
+        minDistance={13}
+        onChange={(value, index) => {
+          changeSlider ({ value, index })
+        }}
+      />
     </div>
   )
 }
